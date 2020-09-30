dba0338bb34c2e53bce5b865238ef750bbb5541b
Darren <darren@MacBook-Pro.lan>
2020-09-30 20:51:20 +0200

    initial commit

---
+++ assets/ReplaceCharacterInString.playground/Contents.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+// This is not mutating
+let testString = "This is a test string"
+var testStringReplacedOccurences = testString.replacingOccurrences(of: "is",
+                                                                   with: "replaced")
+
+print("testStringReplacedOccurences: ", testStringReplacedOccurences)
+
+// https://developer.apple.com/documentation/foundation/nsregularexpression
+var testStringOccurences2 = testString.replacingOccurrences(of: #"\bis"#,
+                                                            with: "was",
+                                                            options: .regularExpression,
+                                                            range: nil)
+
+print("testStringOccurences2: ", testStringOccurences2)
+
+// OUTPUT: This was a test string
+
+if let range = testString.range(of: "is") {
+    let testString2 = testString.replacingCharacters(in: range,
+                                                     with: "at")
+    
+    print("testString2: ", testString2)
+    
+    // OUTPUT: That is a test string
+}
+
+// Mutating
+var substringReplace = "Replace a substring"
+let endIndex = substringReplace.index(substringReplace.startIndex,
+                                      offsetBy: 8)
+substringReplace.replaceSubrange(...endIndex,
+                                 with: "Replaced the")
+print("substringReplace", substringReplace)
+// OUTPUT: Replaced the substring
